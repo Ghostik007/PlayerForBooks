8604113e50d68a7d3b55a593b5748852e8332f62
ghostik007 <ghostik007@gmail.com>
2019-04-08 23:27:28 +0500

    Hello, Kitty testing.

--- app/src/main/java/com/garenskihdl/playerforbooks/MainActivity.java
+++ app/src/main/java/com/garenskihdl/playerforbooks/MainActivity.java
@@ -2,12 +2,25 @@ package com.garenskihdl.playerforbooks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText getDataFromNewCatNamePT;
+    private TextView setTextToCatNameTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTextToCatNameTV = findViewById(R.id.CatNameTV);
+        getDataFromNewCatNamePT = findViewById(R.id.NewCatNamePT);
+    }
+
+    public void SetNewName(View view) {
+        int CatNameSize =  getDataFromNewCatNamePT.getText().length();
+        setTextToCatNameTV.setText( "Hello, " + (( CatNameSize > 0 ) ?  getDataFromNewCatNamePT.getText() : "Kitty") + "!"  );
     }
 }
